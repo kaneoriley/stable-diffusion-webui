e9f7d0d768bac05aa149deea0b1f764f1f119abf
Kane O'Riley <git@kaneoriley.com>
2023-09-26 13:35:03 +0930

    Personal config sync

--- webui-macos-env.sh
+++ webui-macos-env.sh
@@ -14,4 +14,8 @@ export COMMANDLINE_ARGS="--skip-torch-cuda-test --upcast-sampling --no-half-vae 
 export TORCH_COMMAND="pip install torch==2.1.0 torchvision==0.16.0"
 export PYTORCH_ENABLE_MPS_FALLBACK=1
 
-####################################################################
+# Kane modifications
+export COMMANDLINE_ARGS="$COMMANDLINE_ARGS --ckpt-dir /Users/kane/Documents/Stable-Diffusion/models/ckpt"
+export COMMANDLINE_ARGS="$COMMANDLINE_ARGS --embeddings-dir /Users/kane/Documents/Stable-Diffusion/models/embeddings"
+export COMMANDLINE_ARGS="$COMMANDLINE_ARGS --lora-dir /Users/kane/Documents/Stable-Diffusion/models/lora"
+export COMMANDLINE_ARGS="$COMMANDLINE_ARGS --vae-dir /Users/kane/Documents/Stable-Diffusion/models/vae"
